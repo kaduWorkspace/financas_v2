2b0f5b5482cd04d13d22cb8e38fde6a22b45fa7c
carlos.junior <carlos.junior@nexcore.com.br>
2025-01-23 20:35:17 -0300

    :construction: Adicionada rota de fallback para /

--- routes/web.go
+++ routes/web.go
@@ -10,11 +10,18 @@ import (
 
 func Web() {
     financasController := controllers.NewFinancasController()
+    facades.Route().Fallback(func (ctx http.Context) http.Response {
+        return ctx.Response().Redirect(http.StatusSeeOther, "/")
+    })
     facades.Route().Get("/", func(ctx http.Context) http.Response {
-        return ctx.Response().View().Make("financas.v2.tmpl", map[string]any{
-            "data_inicial": time.Now().Format("2006-01-02"),
-            "csrf": ctx.Request().Headers().Get("X-CSRF-Token"),
-        })
+        contexto_view := map[string]any{}
+        contexto_view["data_inicial"] = time.Now().Format("2006-01-02")
+        contexto_view["csrf"] = ctx.Request().Headers().Get("X-CSRF-Token")
+        erro := ctx.Request().Query("erro")
+        if  erro != "" {
+            contexto_view["panic"] = erro
+        }
+        return ctx.Response().View().Make("financas.v2.tmpl", contexto_view)
     })
     facades.Route().Post("/calcular", financasController.CalcularWeb)
 }
