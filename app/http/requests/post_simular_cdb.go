f64c6dfb58354bfe44c4682c606d8ec529c0c62a
carlos.junior <carlos.junior@nexcore.com.br>
2025-01-29 21:59:11 -0300

    :construction: Atualizado front

---
+++ app/http/requests/post_simular_cdb.go
@@ -0,0 +1,75 @@
+package requests
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/goravel/framework/contracts/http"
+	"github.com/goravel/framework/contracts/validation"
+)
+
+type PostSimularCdb struct {
+    ValorInicial          float64 `json:"valor_inicial" form:"valor_inicial" form:"valor_inicial"`
+    ValorAporte          float64 `json:"valor_porte" form:"valor_aporte" form:"valor_aporte"`
+    ValorTaxaAnual          string `json:"data_taxa_anual" form:"data_taxa_anual" form: valor_taxa_anual`
+    DataInicial           string `json:"data_inicial" form:"data_inicial" form:"data_inicial"`
+    DataFinal             string `json:"data_final" form:"data_final" form:"data_final"`
+    DiasLiquidezPorAno string `json:"dias_liquidez_por_ano" form:"periodos_liquidez_por_ano"`
+}
+func (r *PostSimularCdb) ValidarData() error {
+	_, err := time.Parse("02/01/2006", r.DataInicial) // "02" é o dia, "01" é o mês, "2006" é o ano no Go
+	if err != nil {
+		//return errors.New("Data inicial, o formato deve ser d/m/Y")
+        return errors.New("Erro inexperado")
+	}
+	_, err = time.Parse("02/01/2006", r.DataFinal) // "02" é o dia, "01" é o mês, "2006" é o ano no Go
+	if err != nil {
+		//return errors.New("Data final, o formato deve ser d/m/Y")
+        return errors.New("Erro inexperado")
+	}
+    timeInicial, err := time.Parse("02/01/2006", r.DataInicial)
+    if err != nil {
+        return errors.New("Erro inexperado")
+    }
+    timeFinal, err := time.Parse("02/01/2006", r.DataFinal)
+    if err != nil {
+        return errors.New("Erro inexperado!")
+    }
+    if timeFinal.Before(timeInicial) {
+        //return errors.New("Data inicial deve ser menor do que data final!")
+        return errors.New("Erro inexperado!")
+    }
+    limite := 20
+    if (timeFinal.Year() - timeInicial.Year()) > limite {
+        return errors.New(fmt.Sprintf("Limite de %d anos excedido!", limite))
+    }
+	return nil
+}
+
+func (r *PostSimularCdb) Authorize(ctx http.Context) error {
+	return nil
+}
+
+func (r *PostSimularCdb) Rules(ctx http.Context) map[string]string {
+	return map[string]string{
+        "valor_inicial":            "numeric",
+        "valor_aporte":            "numeric",
+        "data_inicial":             "required|string",
+        "data_final":               "required|string",
+        "valor_taxa_anual":         "required|string",
+        "dias_liquidez_por_ano": "required|numeric",
+    }
+}
+
+func (r *PostSimularCdb) Messages(ctx http.Context) map[string]string {
+	return map[string]string{}
+}
+
+func (r *PostSimularCdb) Attributes(ctx http.Context) map[string]string {
+	return map[string]string{}
+}
+
+func (r *PostSimularCdb) PrepareForValidation(ctx http.Context, data validation.Data) error {
+	return nil
+}
---
+++ public/scripts/validarInputsFinancasV2.js
@@ -0,0 +1,100 @@
+const valor_taxa_anual = document.getElementById('valor_taxa_anual')
+const valor_aporte = document.getElementById('valor_aporte')
+const dias_liquidez = document.getElementById('tipo_dias_liquidez_por_ano')
+const form = document.getElementById('formulario_calcular')
+const inputsPossiveis = [...form.elements].filter(input => !input.dataset.ignore_input)
+const data_final_opcoes = document.getElementById('data_final_opcao')
+const data_final_especifico_wrapper = document.getElementById('data_especifica_wrapper')
+const data_final_especifico_input = document.getElementById('data_final')
+
+data_final_opcoes.addEventListener('change', ({target:{value}}) => {
+    value == "data_especifica"
+        ? data_final_especifico_wrapper.classList.remove('hidden')
+        : data_final_especifico_wrapper.classList.add('hidden')
+})
+const formatar_data = (data) => {
+    const ano = data.getFullYear();
+    const mes = String(data.getMonth() + 1).padStart(2, "0"); // Mês começa em 0
+    const dia = String(data.getDate()).padStart(2, "0");
+    const data_resultado = `${ano}-${mes}-${dia}`;
+    return data_resultado
+}
+const incrementar_data = (quantidade = 6, tipo = "meses") => {
+    const data = new Date();
+    tipo == "meses"
+        ? data.setMonth(data.getMonth() + quantidade)
+        : data.setFullYear(data.getFullYear() + quantidade)
+    return formatar_data(data);
+}
+const buscarValoresInput = () => {
+    return inputsPossiveis.reduce((acc, item) => {
+            acc[item.name] = item.type == 'number' ? parseInt(item.value) : item.value
+            return acc;
+    }, {})
+}
+const validarValoresInputs = (inputs, validarNull = false) => {
+    const {valor_inicial, aporte_mensal, aporte_semestral, data_final, tipo_frequencia_aporte, valor_aumento_aporte} = inputs
+    const erros = [];
+    if(valor_inicial < 0 || (validarNull && ["",null,false].includes(valor_inicial))) erros.push(["valor_inicial","Valor inicial inválido"]);
+    if(aporte_mensal > 1000000) erros.push(["aporte_mensal","Aporte mensal muito alto"]);
+    if(!data_final) erros.push(["data_final","Data final inválida"]);
+    return erros.length ? erros : false;
+}
+inputsPossiveis.forEach(item => {
+    item.addEventListener('input', event => {
+        const errorSpan = document.getElementById(`error_${item.id}`)
+        if(errorSpan)
+            errorSpan.classList.add('hidden');
+        let validacao = validarValoresInputs(buscarValoresInput(), false);
+        if(!validacao) return;
+        const validacaoInput = validacao.find(valid => valid[0] == item.name);
+        if(!validacaoInput) return;
+        errorSpan.innerText = validacaoInput[1];
+        errorSpan.classList.remove('hidden')
+        return;
+    });
+})
+document.addEventListener("DOMContentLoaded", () => {
+    if(!form) return
+    const data_inicial_input = document.getElementById("data_inicial")
+    data_inicial_input.value = formatar_data(new Date())
+    for (const input of form.elements) {
+        if (input.name) {
+            const savedValue = sessionStorage.getItem(input.name);
+            if (savedValue) {
+                input.value = savedValue;
+            }
+        }
+    }
+    if(data_final_opcoes.value == "data_especifica") {
+        data_final_especifico_wrapper.classList.remove('hidden')
+    }
+    form.addEventListener("input", function (event) {
+        const { name, value } = event.target;
+        if (name) {
+            sessionStorage.setItem(name, value);
+        }
+    });
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        if(data_final_opcoes.value !== "data_especifica") {
+            const tipo = data_final_opcoes.value == "6" ? "meses" : "anos";
+            data_final_especifico_input.value = incrementar_data(parseInt(data_final_opcoes.value), tipo)
+        }
+        const validacoes = validarValoresInputs(buscarValoresInput(), true);
+        if(validacoes) {
+            validacoes.forEach(validacao => {
+                const errorSpan = document.getElementById(`error_${validacao[0]}`)
+                errorSpan.innerText = validacao[1];
+                errorSpan.classList.remove('hidden')
+            })
+            return
+        }
+        inputsPossiveis.filter(input => input.type == "number").forEach(input => {
+            if(input.value === "") {
+                input.value = 0.0
+            }
+        })
+        form.submit()
+    })
+})
