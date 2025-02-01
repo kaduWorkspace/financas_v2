6e0675aa80611b7e690ea72594bc52518c7a276e
carlos.junior <carlos.junior@nexcore.com.br>
2025-02-01 16:07:31 -0300

    :cosntruction: Adicionado mascara de input

--- public/scripts/validarInputsFinancasV2.js
+++ public/scripts/validarInputsFinancasV2.js
@@ -1,6 +1,9 @@
 const valor_taxa_anual = document.getElementById('valor_taxa_anual')
+const valor_taxa_anual_input = document.getElementById('valor_taxa_anual_input')
 const valor_aporte = document.getElementById('valor_aporte')
+const valor_aporte_input = document.getElementById('valor_aporte_input')
 const valor_inicial = document.getElementById('valor_inicial')
+const valor_inicial_input = document.getElementById('valor_inicial_input')
 const dias_liquidez = document.getElementById('tipo_dias_liquidez_por_ano')
 const form = document.getElementById('formulario_calcular')
 const inputsPossiveis = [...form.elements].filter(input => !input.dataset.ignore_input)
@@ -8,6 +11,9 @@ const inputs_por_nome = inputsPossiveis.reduce((acc, curr) => {
     acc[curr.name] = curr
     return acc
 }, {})
+const tirar_mascara = (v, p) => {
+    return Number(v.replaceAll('.','').replaceAll(',','.').replaceAll(p,'').trim()) ?? 0
+}
 const inputs_por_nome_valor = () => inputsPossiveis.reduce((acc, curr) => {
     acc[curr.name] = curr.value
     return acc
@@ -18,20 +24,65 @@ const data_final_especifico_input = document.getElementById('data_final')
 const to_valor_monetario = string => {
     return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(string);
 }
-const moneyMask = (value) => {
-    value = value.replace('.', '').replace(',', '').replace(/\D/g, '')
-    const options = { minimumFractionDigits: 2 }
-    const result = new Intl.NumberFormat('pt-BR', options).format(
-        parseFloat(value) / 100
-    )
-    console.log(result)
-    if(result[0] === "0") {
-        let [zero, ...rest] = result.split('')
+function removerZerosIniciais(str) {
+    const regex = /^0*(\d+)$/;
+    const match = str.match(regex);
+    if (match) {
+        return match[1];
+    } else {
+        return str
     }
-    return 'R$ ' + result
 }
-const event_money_mask = e => {
-    e.target.value = moneyMask(e.target.value)
+const mascara_monetaria = valor => {
+    valor = valor.replace(/\D/g, '')
+    valor = removerZerosIniciais(valor)
+    let valor_split = valor.split('')
+    if (valor_split.length == 0) {
+        return "0,00"
+    }
+    if (valor_split.length == 1) {
+        return `0,0${valor_split.pop()}`
+    }
+    if (valor_split.length == 2) {
+        return `0,${valor_split.shift()}${valor_split.shift()}`
+    }
+    if (valor_split.length == 3) {
+        return `${valor_split.shift()},${valor_split.shift()}${valor_split.shift()}`
+    }
+    let centavos = [valor_split.pop(), valor_split.pop()].reverse().join("");
+    let grupos_de_tres = [[]];
+    let cont = 0;
+    let cont_grupo = 0;
+    let resto = valor_split.length % 3;
+    let primeiros = []
+    while (resto > 0) {
+        primeiros.push(valor_split.shift())
+        resto--
+    }
+    while(cont < valor_split.length) {
+        const atual = valor_split[cont]
+        if(grupos_de_tres[cont_grupo].length == 3) {
+            grupos_de_tres.push([])
+            cont_grupo++;
+        }
+        grupos_de_tres[cont_grupo].push(atual);
+        cont++;
+    }
+    let valor_formatado = grupos_de_tres.map(grupo => grupo.join('')).join('.')
+    let resultado = "";
+    if(primeiros.length) {
+        if (valor_formatado !== "") {
+            resultado = primeiros.join("") + "." + valor_formatado + "," + centavos;
+        } else {
+            resultado = primeiros.join("") + "," + centavos;
+        }
+    } else {
+        resultado = valor_formatado + "," + centavos;
+    }
+    return resultado;
+}
+const evento_mascara_monetaria = e => {
+    e.target.value = "R$ " + mascara_monetaria(e.target.value)
 }
 data_final_opcoes.addEventListener('change', ({target:{value}}) => {
     value == "data_especifica"
@@ -59,10 +110,10 @@ const buscarValoresInput = () => {
     }, {})
 }
 const validarValoresInputs = (inputs, validarNull = false) => {
-    const {valor_inicial, aporte_mensal, aporte_semestral, data_final, tipo_frequencia_aporte, valor_aumento_aporte} = inputs
+    const { valor_inicial, valor_aporte, data_final } = inputs
     const erros = [];
-    if(valor_inicial < 0 || (validarNull && ["",null,false].includes(valor_inicial))) erros.push(["valor_inicial","Valor inicial inválido"]);
-    if(aporte_mensal > 1000000) erros.push(["aporte_mensal","Aporte mensal muito alto"]);
+    if(tirar_mascara(valor_inicial, "R$") < 0 || (validarNull && ["",null,false].includes(valor_inicial))) erros.push(["valor_inicial","Valor inicial inválido"]);
+    if(tirar_mascara(valor_aporte, "R$") > 1000000) erros.push(["aporte_mensal","Aporte mensal muito alto"]);
     if(!data_final) erros.push(["data_final","Data final inválida"]);
     return erros.length ? erros : false;
 }
@@ -101,9 +152,11 @@ document.addEventListener("DOMContentLoaded", () => {
             sessionStorage.setItem(name, value);
         }
     });
-    console.log(valor_aporte)
-    valor_aporte.addEventListener('keydown', event_money_mask)
-    valor_inicial.addEventListener('keydown', event_money_mask)
+    valor_aporte.addEventListener('input', evento_mascara_monetaria)
+    valor_inicial.addEventListener('input', evento_mascara_monetaria)
+    valor_taxa_anual.addEventListener('input', e => {
+        e.target.value = "% " + mascara_monetaria(e.target.value)
+    })
     form.addEventListener('submit', event => {
         event.preventDefault();
         if(data_final_opcoes.value !== "data_especifica") {
@@ -124,7 +177,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 input.value = 0.0
             }
         })
-        console.log(inputs_por_nome_valor())
-//       form.submit()
+        let taxa_anual_v = Number(valor_taxa_anual.value.replaceAll('.','').replaceAll(',','.').replaceAll('%','').trim()) ?? 0
+        let valor_aporte_v = Number(valor_aporte.value.replaceAll('.','').replaceAll(',','.').replaceAll('R$','').trim()) ?? 0
+        let valor_inicial_v = Number(valor_inicial.value.replaceAll('.','').replaceAll(',','.').replaceAll('R$','').trim()) ?? 0
+        valor_aporte_input.value = !!valor_aporte_v ? valor_aporte_v : 0
+        valor_taxa_anual_input.value = !!taxa_anual_v ? taxa_anual_v : 0
+        valor_inicial_input.value = !!valor_inicial_v ? valor_inicial_v : 0
+        form.submit()
     })
 })
