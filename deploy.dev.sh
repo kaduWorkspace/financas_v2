987c3a0fdc28a023dcb73f49d008e538517efd7b
kaduhod <carlosjr.ribas@gmail.com.br>
2025-05-03 21:10:07 -0300

    :hammer: Fix: sh para rodar container

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ssh -t deployer@172.17.0.2 <<EOF
+    newgrp docker
     docker pull kaduhod/fin
-    docker stop cdb
-    docker rm cdb
+    docker stop cdb || true
+    docker rm cdb || true
     docker run --name cdb -d -p 3000:3000 kaduhod/fin
 EOF
