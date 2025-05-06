4ca67b1d135467f00f6be5ba28ec23ba9fc2e97b
kaduhod <carlosjr.ribas@gmail.com.br>
2025-05-06 14:31:34 -0300

    :hammer: Feat: Adicionado testes e funcao para calculo de future value of a series

--- app/core/modules/financas/futureValueOfASeries.go
+++ app/core/modules/financas/futureValueOfASeries.go
@@ -1,14 +1,33 @@
 package financas
+
+import (
+	"github.com/shopspring/decimal"
+)
+
 // interestRateDecimal: decimal interest rate per period
-// netDays: number of days until payment
-// years: investment duration in years
+// periods: number of days until payment
+// months: investment duration in years
 // contributionAmount: amount of each contribution
-// contributionOnFirstDay: whether contribution is made at start of period
+// ContributionOnFirstDay: whether contribution is made at start of period
 type FutureValueOfASeries struct {
-    initialValue float64
-    interestRateDecimal float64
-    netDays             float64
-    years               float64
-    contributionAmount  float64
-    contributionOnFirstDay bool
+    InterestRateDecimal float64
+    Periods             float64
+    ContributionAmount  float64
+    ContributionOnFirstDay bool
+}
+func (self * FutureValueOfASeries) Calculate() float64 {
+    decimalContribuitionAmount := decimal.NewFromFloat(self.ContributionAmount)
+    decimalPeriods := decimal.NewFromFloat(self.Periods)
+    decimalInterestRateDecimal := decimal.NewFromFloat(self.InterestRateDecimal)
+    taxPerPeriods := decimalInterestRateDecimal.Round(16).Div(decimalPeriods).Round(16)
+    growthFactor := decimal.NewFromInt(1).Add(taxPerPeriods).Round(16).Pow(decimalPeriods).Round(16)
+    growthFactorPerTaxPerPeridos := growthFactor.Sub(decimal.NewFromInt(1)).Div(taxPerPeriods)
+    result := decimalContribuitionAmount.Mul(growthFactorPerTaxPerPeridos).Round(16)
+    if self.ContributionOnFirstDay {
+        firstPeriod := decimal.NewFromInt(1).Add(taxPerPeriods)
+        futureValue, _ := result.Mul(firstPeriod).Round(16).Float64()
+        return futureValue
+    }
+    futureValue, _ := result.Round(16).Float64()
+    return futureValue
 }
